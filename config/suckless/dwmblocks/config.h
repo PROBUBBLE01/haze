1f2909ce27fd3ff6dfebd1a21989a4774ddde9a1
Sreejith <46400271+PROBUBBLE01@users.noreply.github.com>
2022-08-06 18:34:57 +0530

    updated configs

---
+++ config/suckless/dwmblocks/config.h
@@ -0,0 +1,10 @@
+#define CMDLENGTH 45
+#define DELIMITER "  "
+#define CLICKABLE_BLOCKS
+
+const Block blocks[] = {
+        BLOCK("sb-traffic",  3,    0),
+        BLOCK("sb-volume",   0,    1),
+	BLOCK("sb-battery",  5,    0),
+	BLOCK("sb-date",     30,   0)
+};
